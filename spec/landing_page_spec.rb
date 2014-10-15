2155a1c56e43ccd2e71dcf9a73ba139851835d5a
Grayson Wright <wright.grayson@gmail.com>
2014-10-15 18:07:59 -0400

    Add a spec for counting the events

--- spec/landing_page_spec.rb
+++ spec/landing_page_spec.rb
@@ -5,4 +5,16 @@ describe "Landing Page" do
     visit '/'
     expect(page).to have_content('Global Day of Coderetreat')
   end
+
+  describe "#event_count" do
+    it "Counts the cities listed in the locations.json file" do
+      expect(File).to receive(:read).with("public/data/locations.json").
+        and_return( [{city: "Chicago", coords: [1, 2]},
+                     {city: "Shanghai", coords: [3, 4]}].to_json)
+
+        visit '/'
+
+        expect(page).to have_content("2 registered events")
+    end
+  end
 end
