b5b8822c264caea41049dd456836392b6594a03f
Grayson Wright <wright.grayson@gmail.com>
2014-10-15 18:27:29 -0400

    Create pending specs for the landing page

--- spec/landing_page_spec.rb
+++ spec/landing_page_spec.rb
@@ -6,8 +6,20 @@ describe "Landing Page" do
     expect(page).to have_content('Global Day of Coderetreat')
   end
 
+  it "displays sponsors"
+  it "displays links to hosting resources"
+
+  it "displays social media links"
+  it "displays links to the coderetreat.org site"
+
+  it "describes CoderDojo" do
+    visit "/"
+    pending
+    expect(page).to have_content("CoderDojo")
+  end
+
   describe "#event_count" do
-    it "Counts the cities listed in the locations.json file" do
+    it "Displays a count of the cities in the locations.json file" do
       expect(File).to receive(:read).with("public/data/locations.json").
         and_return( [{city: "Chicago", coords: [1, 2]},
                      {city: "Shanghai", coords: [3, 4]}].to_json)
